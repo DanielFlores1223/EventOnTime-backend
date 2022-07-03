b65be6ea2681dc4db48faca779f6e0a4da6e8bc0
DanielFlores1223 <dany-232000@hotmail.com>
2022-07-03 01:05:03 -0500

    CRUD users finished

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,69 @@ const { getJsonRes } = require('../helpers/json-responses');
 const bcryptjs = require('bcryptjs')
 const { User } = require('../models');
 
+const getAll = async ( req = request, res = response ) => {
+
+     try {
+          const { limit = 5, from = 0, pagination = 'true' } = req.query;
+
+          let result;
+          const p = ( pagination.toLowerCase() === 'true' );
+          
+          if( p ) {
+               result = await Promise.all([
+                    User.countDocuments( { status: true } ),
+                    User.find( { status: true } )
+                        .skip( Number( from ) )
+                        .limit( Number( limit ) )
+               ]);
+          
+          } else {
+               result = await Promise.all([
+                    User.countDocuments( { status: true } ),
+                    User.find( { status: true } )
+               ]);
+          }
+     
+          const [ total, users ] = result;
+
+          const resultJson = { total, users };
+
+          res.status( 200 ).json( getJsonRes( true, 'Usuarios encontrados correctamente', resultJson ) );
+
+     } catch (error) {
+          console.log(error);
+          res.status( 400 ).send( getJsonRes( false, 'Algo salió mal...' ) );
+     }
+
+}
+
+const getById = async ( req = request, res = response ) => {
+
+     try {
+          const { id } = req.params;
+          const user = await User.findById( id );
+          res.status( 200 ).json( getJsonRes( true, 'La información del usuario se encontró correctamente', user ) );
+          
+     } catch (error) {
+          console.log(error);
+          res.status( 400 ).send( getJsonRes( false, 'Algo salió mal...' ) );
+     }
+}
+
+const getInfoProfile = async ( req = request, res = response ) => {
+
+     try {
+          const { _id } = req.user;
+          const user = await User.findById( _id );
+          res.status( 200 ).json( getJsonRes( true, 'La información del usuario se encontró correctamente', user ) );
+     
+     } catch (error) {
+          console.log(error);
+          res.status( 400 ).send( getJsonRes( false, 'Algo salió mal...' ) );
+
+     }
+}
+
 const create = async ( req = request, res = response ) => {
      try {
           const { name, email, password, status, account, role } = req.body;
@@ -25,8 +88,49 @@ const create = async ( req = request, res = response ) => {
      }
 }
 
+// Update profile of the user
+const update = async ( req = request, res = response ) => {
+
+     try {
+          const { google, favorites, status, account, role, _id, ...rest } = req.body;
+
+          if( rest.password ) {
+               const salt = bcryptjs.genSaltSync();
+               rest.password = bcryptjs.hashSync( rest.password, salt );
+          }
+
+          const user = await User.findByIdAndUpdate( req.user._id, rest, { new: true } );
+
+          res.status( 200 ).send( getJsonRes( true, `Tu perfil se modificó correctamente`, user ) );
+
+     } catch (error) {
+          console.log( error );
+
+          if( error.message.includes( 'E11000 duplicate key' ) )
+               return res.status( 400 ).send( getJsonRes( false, 'El correo que intentó registrar ya existe' ) );
+
+          res.status( 400 ).send( getJsonRes( false, 'Algo salió mal...' ) );
+     }
+}
+
+const deleteOne = async ( req = request, res = response ) => {
+     
+     try {
+          const { id } = req.params; 
+          const user = await User.findByIdAndUpdate( id, { status: false }, { new: true } );
+          res.status( 200 ).send( getJsonRes( true, 'El usuario se eliminó correctamente', user ) );
+     
+     } catch (error) {
+          console.log(error);
+          res.status( 400 ).send( getJsonRes( false, 'Algo salió mal...' ) );
+     }
+}
 
 module.exports = {
+     getAll,
+     getById,    
+     getInfoProfile,
      create,
-
+     update,
+     deleteOne,
 }
--- routes/user.js
+++ routes/user.js
@@ -5,20 +5,40 @@ const { RolesEnum } = require('../helpers/enums');
 const { isValidRole, 
         emailExist, 
         isValidTypeAccount,
-        isValidRoleWithoutAdmin } = require('../helpers');
+        isValidRoleWithoutAdmin, 
+        documentExist} = require('../helpers');
 const { validateJWT, 
         validateRole, 
         validateFields } = require('../middlewares');
 
 const router = Router();
 
+router.get( '/', [
+     validateJWT,
+     check('limit', 'El limite debe ser númerico').optional().isNumeric(),
+     check('from', 'El inicio debe ser númerico').optional().isNumeric(),
+     validateFields
+], controller.getAll );
+
+router.get( '/profile', [
+     validateJWT
+], controller.getInfoProfile );
+
+router.get( '/:id', [
+        validateJWT,
+        check( 'id', 'No es un id válido' ).isMongoId(),
+        check( 'id' ).custom( id => documentExist( id, 'User' ) ),
+        validateFields
+   ], controller.getById );
+
 // This route is for a Admin wants to register a User
 router.post( '/', [
      validateJWT,
      validateRole( RolesEnum.admin ),
      check( 'name', 'El nombre es obligatorio').not().isEmpty(),
-     check( 'email', 'El correo no es valido' ).not().isEmpty(),
+     check( 'email', 'El correo no es válido' ).not().isEmail(),
      check( 'email' ).custom( emailExist ),
+     check( 'password', 'La contraseña es obligatoria' ).not().isEmpty(),
      check( 'role', 'El rol es obligatorio' ).not().isEmpty(),
      check( 'role' ).custom( isValidRole ),
      check( 'account', 'El rol es obligatorio' ).not().isEmpty(),
@@ -29,13 +49,31 @@ router.post( '/', [
 // This route is for when the user wants to register himself from landing page
 router.post( '/register', [
         check( 'name', 'El nombre es obligatorio').not().isEmpty(),
-        check( 'email', 'El correo no es valido' ).not().isEmpty(),
+        check( 'email', 'El correo no es válido' ).not().isEmail(),
         check( 'email' ).custom( emailExist ),
+        check( 'password', 'La contraseña es obligatoria' ).not().isEmpty(),
         check( 'role', 'El rol es obligatorio' ).not().isEmpty(),
         check( 'role' ).custom( isValidRoleWithoutAdmin ),
         check( 'account', 'El rol es obligatorio' ).not().isEmpty(),
         check( 'account' ).custom( isValidTypeAccount ),
         validateFields,
    ], controller.create );
+
+
+router.put( '/profile', [
+    validateJWT,
+    check( 'name', 'El nombre no debe estar vacío').optional().not().isEmpty(),
+    check( 'password', 'La contraseña no debe estar vacía').optional().not().isEmpty(),
+    check( 'email', 'El correo no es válido' ).optional().isEmail(),
+    validateFields,
+], controller.update );
+
+router.delete( '/:id', [
+    validateJWT,
+    validateRole( RolesEnum.admin ),
+    check( 'id', 'No es un id válido' ).isMongoId(),
+    check( 'id' ).custom( id => documentExist( id, 'User' ) ),
+    validateFields,
+], controller.deleteOne );
  
 module.exports = router;
